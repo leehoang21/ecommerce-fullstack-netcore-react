0c0ebffb4d8c50d98d4c1f7f8ab4b9351d861ac1
hoanglee21 <hoanglethanh2132003@gmail.com>
2024-10-20 23:19:49 +0700

    first commit

---
+++ frontend/src/utils/api/variables.js
@@ -0,0 +1,8 @@
+export const variables = {
+    BASE_URL: "http://online-shop.hoanglee.id.vn:5214/api/",
+    USER_API: "http://online-shop.hoanglee.id.vn:5214/api/user",
+    PRODUCT_API: "http://online-shop.hoanglee.id.vn:5214/api/product",
+    ORDER_API: "http://online-shop.hoanglee.id.vn:5214/api/order",
+    PRODUCTSIZE_API: "http://online-shop.hoanglee.id.vn:5214/api/productsize",
+    ORDERITEM_API: "http://online-shop.hoanglee.id.vn:5214/api/orderitem",
+}
